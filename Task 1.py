7e4833781d482b5f7b421ed18606d4cae1c53ed8
Darya <darecc@yandex.ru>
2022-10-16 22:36:25 +0400

    add Task1

--- Task 1.py
+++ Task 1.py
@@ -1,9 +1,14 @@
 # Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
 
-def rec_sum(lst):
-    if len(lst)==0:
-        return 0
-    else:
-        return lst[0]+rec_sum(lst[2:])
-        
-print(rec_sum([5,7,10,8,3]))
+List = [2,3,5,9,3]
+a=List[1]
+b=List[3]
+
+print(List)
+list_length=len(List)
+sum = 0
+for i in range(list_length):
+    if i%2 != 0:
+        sum = sum + List[i]
+
+print(f'На нечётных позициях элементы {List[1]} и {List[3]}. Ответ:', sum)
