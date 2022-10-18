89d315c3aef41bac5de07433fe77cd6a4484c314
Darya <darecc@yandex.ru>
2022-10-18 21:54:24 +0400

    add task1

--- Task 1.py
+++ Task 1.py
@@ -5,8 +5,17 @@ List = [2,3,5,9,3]
 print(List)
 list_length=len(List)
 sum = 0
+elements = 'На нечётных позициях элементы '
+num = str(' ')
 for i in range(list_length):
-    if i%2 != 0:
+    if i%2 != 0 and (List[-1] or List[-2]):
         sum = sum + List[i]
+        num = str(List[i])
+        elements = elements + num + '. '
+    elif i%2 != 0:
+        sum = sum + List[i]
+        num = str(List[i])
+        elements = elements + num + ', '
+    
 
-print(f'На нечётных позициях элементы {List[1]} и {List[3]}. Ответ:', sum)
+print(elements + 'Сумма элементов:', sum)
